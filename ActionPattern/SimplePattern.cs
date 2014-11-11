6d0ae07103481136bf1de6192ce07098c4d4aacb
Yoshitaka Kimisaki <y.kimisaki@gmail.com>
2014-11-11 20:10:52 +0900

    SimplePatternを追加

---
+++ ActionPattern/SimplePattern.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.ActionPattern
+{
+    public static class SimplePattern
+    {
+        public static T Pattern<T>(this T source, T predicate, Action<T> action)
+        {
+            if (!Object.Equals(source, null) && Object.Equals(source, predicate)) action(source);
+            return source;
+        }
+
+        public static T Pattern<T>(this T source, Func<T, bool> predicate, Action<T> action)
+        {
+            if (!Object.Equals(source, null) && predicate(source)) action(source);
+            return source;
+        }
+    }
+}
